710b0ddc94304cd3f731f00fc3fa076396007b77
Andrew Downing <ad510n1@gmail.com>
2015-01-17 14:34:17 -0800

    tokenize

--- main.lisp
+++ main.lisp
@@ -4,10 +4,14 @@
       ""
       (concatenate 'string (make-string 1 :initial-element next) (read-tok stream)))))
 
+(defun tokenize (stream)
+  (let ((tok (read-tok stream)))
+    (cons tok (if (string/= tok "END") (tokenize stream) NIL))))
+
 (set-dispatch-macro-character #\# #\?
   #'(lambda (stream subchar numarg)
     ;(first (multiple-value-list (intern (read-tok stream))))
-    (read-tok stream)
+    (tokenize stream)
 ))
 
-(print #?hello )
+(print '#?hello world END )
