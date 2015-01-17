a298682fd402e0a11f8ab2f1e4b272c56a516a94
Andrew Downing <ad510n1@gmail.com>
2015-01-17 14:58:07 -0800

    I can just use read-delimited-list

--- main.lisp
+++ main.lisp
@@ -1,17 +1,6 @@
-(defun read-tok (stream)
-  (let ((next (read-char stream)))
-    (if (char= next #\Space)
-      ""
-      (concatenate 'string (make-string 1 :initial-element next) (read-tok stream)))))
-
-(defun tokenize (stream)
-  (let ((tok (read-tok stream)))
-    (cons tok (if (string/= tok "END") (tokenize stream) NIL))))
-
 (set-dispatch-macro-character #\# #\?
   #'(lambda (stream subchar numarg)
-    ;(first (multiple-value-list (intern (read-tok stream))))
-    (tokenize stream)
+    (read-delimited-list #\# stream)
 ))
 
-(print '#?hello world END )
+(print #?list "hello" "world"#)
