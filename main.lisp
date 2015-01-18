4f4fe524b4e6da72f2277aed3feddf75fc4aa7f1
Andrew Downing <ad510n1@gmail.com>
2015-01-17 19:16:02 -0800

    comments, statement blocks

--- main.lisp
+++ main.lisp
@@ -1,10 +1,19 @@
 (defun transform (code)
-  (if (consp code)
-    (if (consp (second code))
+  (cond
+    ((not (consp code)) code)
+    ((eq (car code) '/*) (transform (find-sym '*/ code)))
+    ((eq (car code) '{) (cons (cons 'progn (transform (cdr code)))
+                              (transform (find-sym '} code))))
+    ((eq (car code) '}) NIL)
+    ((consp (second code))
       (cons (cons (first code) (transform (second code)))
-            (transform (cddr code)))
-      (cons (first code) (transform (cdr code))))
-    code))
+            (transform (cddr code))))
+    (t (cons (car code) (transform (cdr code))))))
+
+(defun find-sym (sym code)
+  (if (eq (car code) sym)
+    (cdr code)
+    (find-sym sym (cdr code))))
 
 (set-dispatch-macro-character #\# #\?
   #'(lambda (stream subchar numarg)
@@ -16,4 +25,10 @@
 
 #? progn
 console.log("Hello, world!");
+{
+  console.log("a");
+  /* console.log("b") */
+  console.log("d");
+}
+console.log("c");
 #
