4715f6a2d21a5e694a3240aca07c516b5c71160f
Andrew Downing <ad510n1@gmail.com>
2015-01-17 16:05:01 -0800

    parse js hello world

--- main.lisp
+++ main.lisp
@@ -1,6 +1,19 @@
+(defun transform (code)
+  (if (consp code)
+    (if (consp (second code))
+      (cons (cons (first code) (transform (second code)))
+            (transform (cddr code)))
+      (cons (first code) (transform (cdr code))))
+    code))
+
 (set-dispatch-macro-character #\# #\?
   #'(lambda (stream subchar numarg)
-    (read-delimited-list #\# stream)
+    (transform (read-delimited-list #\# stream))
 ))
 
-(print #?list "hello" "world"#)
+(defun console.log (msg)
+  (print msg))
+
+#? progn
+console.log("Hello, world!");
+#
